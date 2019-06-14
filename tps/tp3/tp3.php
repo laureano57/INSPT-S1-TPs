20ca986217d39b785558606ffa6d8dcf85ea71bc
laureanotodomal <laureano.57@gmail.com>
2019-06-14 01:58:17 -0300

    TP3 - Separa frontend y backend - Pasa el numero a comprimir por ajax, parsea la respuesta y la carga en el front dinamicamente

--- tps/tp3/tp3.php
+++ tps/tp3/tp3.php
@@ -88,73 +88,11 @@ function maxRleComp($inputNum) {
   );
 }
 
+$output = maxRleComp($_POST['number']);
+
+echo json_encode($output);
+
 // $test = toRle('101000000000000000000111111111111111111111111010101010100100111110000000000000000000000000100010000100000100000010000000100000001', 2);
 // print('<pre>'.print_r($test, true).'</pre>'); die();
 // print('<pre>'.print_r(maxRleComp('0000000000001000000000000000000000000000001000000000000010100000000011100000001111'),true).'</pre>'); die();
-
-
 ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-
-<head>
-  <title>Sistemas de Computacion 1</title>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="../../css/styles.css" type="text/css">
-  <script src="../../libs/jquery-3.4.1.min.js"></script>
-</head>
-
-<body>
-  <div class="wrapper row1">
-    <header id="header">
-      <div id="hgroup">
-        <h1>Sistemas de Computación I</h1>
-        <h2>Trabajos prácticos de la materia</h2>
-        <h2>Grupo 6 - Gonzalez, Olmedo, Stella</h2>
-      </div>
-      <nav>
-        <ul>
-          <li><a id="trabajos-practicos" href="/">Trabajos Prácticos</a></li>
-          <li><a href="../../assets/EnunciadoTPs2019.pdf">Consignas</a></li>
-          <li class="last"><a href="#">Integrantes</a></li>
-        </ul>
-      </nav>
-    </header>
-  </div>
-
-  <div class="wrapper row2">
-    <div id="container" class="clear">
-      <div id="tp-wrapper">
-        <div class="tp-header">
-          <h1>Trabajo Práctico Nº 3</h1>
-          <h2>Compresor RLE</h2>
-        </div>
-        <div class="form-wrapper">
-          <p>Ingrese un número a comprimir:</p>
-          <input type="number" placeholder="Numero entero..." class="tp_numero">
-        </div>
-        <div class="form-wrapper">
-          <p>Resultado:</p>
-          <h3 class="tp_resultado">
-            <?php 
-              $out = maxRleComp('0000000000001000000000000000000000000000001000000000000010100000000011100000001111');
-              echo '<p>' . $out['rleNum'] . '</p>';
-            ?> 
-          </h3>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="wrapper row3">
-    <footer id="footer">
-      <p class="fl_left">INSPT - Tecnicatura Superior en Informática - 2019</p>
-      <p class="validators">
-        <a href="http://validator.w3.org/check?uri=referer"><img src="../../img/valid-html5.png" alt="Valid HTML 5" height="31" width="88"></a>&nbsp;&nbsp;&nbsp;
-        <a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="../../img/vcss.gif" alt="CSS Válido!" height="31" width="88"></a>
-      </p>
-    </footer>
-  </div>
-</body>
-
-</html>
