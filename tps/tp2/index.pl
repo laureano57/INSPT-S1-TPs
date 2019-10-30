aa9e304d03dd2f4b1df10cb79c55b3839b2f246b
laureanotodomal <laureano.57@gmail.com>
2019-10-30 01:37:43 -0300

    Fix

--- tps/tp2/index.pl
+++ tps/tp2/index.pl
@@ -62,7 +62,6 @@ print<<EOF;
         <ul>
           <li><a id="trabajos-practicos" href="/">Trabajos Prácticos</a></li>
           <li><a href="/consignas.html">Consignas</a></li>
-          
         </ul>
       </nav>
     </header>
